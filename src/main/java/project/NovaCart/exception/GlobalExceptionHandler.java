76f8dd6ec468054a4b1d01ef55957947b6d4a077
dinesh-babu-S <dineshxbabu@gmail.com>
2026-07-08 11:34:17 +0530

    Add UnauthorizedException handler to GlobalExceptionHandler

--- src/main/java/project/NovaCart/exception/GlobalExceptionHandler.java
+++ src/main/java/project/NovaCart/exception/GlobalExceptionHandler.java
@@ -31,7 +31,13 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
-
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<Map<String, String>> handleUnauthorized(
+            UnauthorizedException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("message", ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.FORBIDDEN);
+    }
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidation(
             MethodArgumentNotValidException ex) {
